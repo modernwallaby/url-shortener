455f104db3a9b22adf6b9e5eaac808316ad09c49
Seth Baur <sethbaur@gmail.com>
2021-03-20 11:41:48 -0400

    Url validation

--- components/UrlForm/UrlForm.tsx
+++ components/UrlForm/UrlForm.tsx
@@ -2,20 +2,31 @@ import React, { FC, useState, ChangeEvent, SyntheticEvent } from 'react';
 import Input from './Input';
 import Button from './Button';
 
+const urlRegex = /https?:\/\/.+\..+/;
+
 interface UrlFormProps {
   onSubmit(url: string): void;
 }
 
 const UrlForm: FC<UrlFormProps> = ({ onSubmit }) => {
   const [url, setUrl] = useState<string>('');
+  const [error, setError] = useState<boolean>(false);
   const submit = (e: SyntheticEvent) => {
     e.preventDefault();
-    onSubmit(url);
+    if (url.match(urlRegex)) {
+      setError(false);
+      onSubmit(url);
+    } else {
+      setError(true);
+    }
   };
 
   return (
     <form>
       <Input type="text" name="url" value={url} onChange={(e: ChangeEvent<HTMLInputElement>) => setUrl(e.target.value)} />
+      {error &&
+        <p>Invalid URL</p>
+      }
       <Button onClick={submit}>Shorten!</Button>
     </form>
   );
